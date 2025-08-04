b6575a87873bbaec95ee4b2f10c4437d30f7d57e
Placeless <thinkoverheat@gmail.com>
2025-08-04 20:16:44 +0900

    Implement error trait bounds

--- src/error.rs
+++ src/error.rs
@@ -26,21 +26,24 @@ impl<I2cE> From<I2cE> for Sh1107gError<I2cE> {
 
 // src/main.rs または専用のerrorsモジュールに記述
 #[derive(Debug)]
-pub enum AppError {
-    I2cError(embedded_hal::i2c::Error), // I2Cエラーをラップする
+pub enum AppError<E> where E: embedded_hal::i2c::Error {
+    I2cError(E), // I2Cエラーをラップする
     BuilderError,                     // `()`から発生するエラー用
     // 必要に応じて他のエラー型を追加
 }
 
 // arduino_hal::i2c::Error から AppError への変換
-impl From<Error> for AppError {
-    fn from(e: embedded_hal::i2c::Error) -> Self {
+impl<E> From<E> for AppError<E> where E: embedded_hal::i2c::Error {
+    fn from(e: E) -> Self {
         AppError::I2cError(e)
     }
 }
 
 // `E: From<()>` の要件を満たすために From<()> を実装
-impl From<()> for AppError {
+impl<E> From<()> for AppError<E>
+where
+    E: embedded_hal::i2c::Error,
+{
     fn from(_: ()) -> Self {
         AppError::BuilderError
     }
