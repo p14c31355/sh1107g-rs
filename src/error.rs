d5ee1ecf71d5acb1a7953c6fe63e23c89592646b
Placeless <thinkoverheat@gmail.com>
2025-08-04 23:13:43 +0900

    Edit error.rs

--- src/error.rs
+++ src/error.rs
@@ -16,8 +16,16 @@ pub enum Sh1107gError<I2cE> {
 
 // BuilderErrorをSh1107gErrorに変換するためのFrom実装
 // `Sh1107gBuilder::build`メソッドで`?`演算子を使用するために必要
-impl<I2cE> From<BuilderError> for Sh1107gError<I2cE> {
+impl From<BuilderError> for Sh1107gError<core::convert::Infallible> {
     fn from(e: BuilderError) -> Self {
         Sh1107gError::Builder(e)
+    }
+}
+
+// PayloadOverflowをSh1107gErrorに変換するためのFrom実装
+
+impl<I2cE> From<I2cE> for Sh1107gError<I2cE> {
+    fn from(e: I2cE) -> Self {
+        Sh1107gError::I2cError(e)
     }
 }
