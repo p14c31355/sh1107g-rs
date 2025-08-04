169a4a74404147d9b590edaf2a69a588d451fca8
Placeless <thinkoverheat@gmail.com>
2025-08-04 21:15:35 +0900

    Compile lose that error enum conflict

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,5 @@
 // src/error.rs
-
+// ドライバのエラー型を定義
 #[derive(Debug)]
 pub enum BuilderError {
     NoI2cConnected,
@@ -13,8 +13,16 @@ pub enum Sh1107gError<I2cE> {
     I2cError(I2cE),
 }
 
+// I2CエラーをSh1107gErrorに変換するための汎用的なFrom実装
+// `Sh1107gError`にI2Cエラーをラップするため、この実装は必須です。
+impl<I2cE> From<I2cE> for Sh1107gError<I2cE> {
+    fn from(e: I2cE) -> Self {
+        Sh1107gError::I2cError(e)
+    }
+}
+
 // BuilderErrorをSh1107gErrorに変換するためのFrom実装
-// `Sh1107gBuilder::build`メソッドで`?`演算子を使用するために必要
+// `Sh1107gBuilder`の`build`メソッドで`?`演算子を使用するために必要
 impl<I2cE> From<BuilderError> for Sh1107gError<I2cE> {
     fn from(e: BuilderError) -> Self {
         Sh1107gError::Builder(e)
