6a456dd5d74768efa751338271e57d4777d24015
Placeless <thinkoverheat@gmail.com>
2025-08-07 14:16:34 +0900

    Integrated compile errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+
 /// SH1107G I2C OLED driver
 pub mod cmds;
 pub mod error;
@@ -11,35 +12,17 @@ pub mod async_;
 
 use embedded_graphics_core::{
     draw_target::DrawTarget,
+    geometry::{Dimensions, Point, Size},
     pixelcolor::BinaryColor,
+    primitives::Rectangle,
     Pixel,
 };
-use core::convert::Infallible;
-use core::result::Result;
-use core::option::Option::{self, Some, None};
-use core::result::Result::Ok;
-use core::iter::IntoIterator;
 
-
-/*
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum DisplayRotation {
-    Rotate0,
-    Rotate90,
-    Rotate180,
-    Rotate270,
-}
-
-// Default process
-impl Default for DisplayRotation {
-    fn default() -> Self {
-        DisplayRotation::Rotate0
-    }
-}
-*/
-
-/// common
-use embedded_graphics_core::geometry::{Dimensions, Point, Size};
+use core::{
+    convert::Infallible,
+    result::Result,
+    option::Option::{self, Some, None},
+};
 
 #[cfg(feature = "debug_log")]
 use dvcdbg::logger::{Logger, SerialLogger};
@@ -51,110 +34,85 @@ pub type DefaultLogger<'a, W> = SerialLogger<'a, W>;
 #[cfg(not(feature = "debug_log"))]
 pub type DefaultLogger = NoopLogger;
 
-pub struct Sh1107g<'a, I2C, L = Logger + ?Sized> {
+pub const DISPLAY_WIDTH: u32 = 128;
+pub const DISPLAY_HEIGHT: u32 = 128;
+pub const BUFFER_SIZE: usize = (DISPLAY_WIDTH * DISPLAY_HEIGHT / 8) as usize;
+
+pub struct Sh1107g<'a, I2C, L: Logger + ?Sized> {
     pub(crate) i2c: I2C,
     pub(crate) address: u8,
-    pub(crate) buffer: [u8; BUFFER_SIZE], // Internal buffer
+    pub(crate) buffer: [u8; BUFFER_SIZE],
     pub(crate) logger: &'a mut L,
-    // Configure in builder to Sh1107g struct
 }
 
-impl <I2C, L> Sh1107g<I2C, L>{
+impl<'a, I2C, L> Sh1107g<'a, I2C, L>
+where
+    L: Logger + ?Sized,
+{
     // Make new driver instance & Degine function called by the builder
     // Initialise the internal buffer when called by builder
-    pub fn new(i2c: I2C, address: u8, logger: L) -> Self {
+    pub fn new(i2c: I2C, address: u8, logger: &'a mut L) -> Self {
         Self {
             i2c,
             address,
-            buffer: [0x00; BUFFER_SIZE], // 全てオフで初期化
+            buffer: [0x00; BUFFER_SIZE],
             logger,
         }
     }
 
-    /// 内部バッファをクリアする
     pub fn clear_buffer(&mut self) {
         self.buffer.iter_mut().for_each(|b| *b = 0x00);
     }
-    
-    #[cfg(feature = "debug_log")]
-    pub fn new_with_logger(i2c: I2C, address: u8, logger: L) -> Self {
-        Self {
-            i2c,
-            address,
-            buffer: [0; BUFFER_SIZE],
-            logger,
-        }
-    }
-
-    pub fn build_log(self) -> Sh1107g<'static, I2C, L> {
-        Sh1107g::new(
-            self.i2c.expect("I2C must be set"),
-            self.address,
-            self.logger.unwrap_or_else(L::default),
-        )
-    }
 }
-// Builder struct
+
 pub struct Sh1107gBuilder<'a, I2C, L: Logger + ?Sized> {
-    i2c: I2C,
-    address: u8,      // Configure default address or choice Option type
-    logger: &'a mut L,
-    // If you can add more settings value rotation: DisplayRotation,etc...
+    i2c: Option<I2C>,
+    address: u8,
+    logger: Option<&'a mut L>,
 }
 
-// Sh1107gBuilder implement block
 impl<'a, I2C, L: Logger + ?Sized> Sh1107gBuilder<'a, I2C, L> {
-    /// Make new builder instance
-    /// Designation default I2C address
-    pub fn new(i2c: I2C, _address: u8, logger: &'a mut L) -> Self {
+    pub fn new(i2c: I2C, logger: &'a mut L) -> Self {
         Self {
-            i2c,
-            address: 0x3C, // default
-            // size: None,
-            // rotation: DisplayRotation::default(),
-            logger,
+            i2c: Some(i2c),
+            address: 0x3C,
+            logger: Some(logger),
         }
     }
-
-    /// Connect I2C
-    pub fn connect_i2c(mut self, i2c: I2C) -> Self {
-        self.i2c = Some(i2c);
-        self
-    }
-
+    
     /// Configure I2C address
     pub fn with_address(mut self, address: u8) -> Self {
         self.address = address;
         self
     }
 
-    pub fn with_logger(mut self, logger: L) -> Self {
-        self.logger = Some(logger);
-        self
+    pub fn build(self) -> Sh1107g<'a, I2C, L> {
+        Sh1107g::new(
+            self.i2c.expect("I2C must be set"),
+            self.address,
+            self.logger.expect("Logger must be set"),
+        )
     }
     // If you need other method, add other setting method, example: size,rotate,etc...
 }
 
-// define display size
-pub const DISPLAY_WIDTH: u32 = 128;
-pub const DISPLAY_HEIGHT: u32 = 128;
-pub const BUFFER_SIZE: usize = (DISPLAY_WIDTH * DISPLAY_HEIGHT / 8) as usize;
-
-// Sh1107g に Dimensions トレイトを実装
-impl<I2C> Dimensions for Sh1107g<I2C> {
-    fn bounding_box(&self) -> embedded_graphics_core::primitives::Rectangle {
-        embedded_graphics_core::primitives::Rectangle::new(
-            Point::new(0, 0),
-            Size::new(DISPLAY_WIDTH, DISPLAY_HEIGHT),
-        )
+impl<'a, I2C, L> Dimensions for Sh1107g<'a, I2C, L>
+where
+    L: Logger + ?Sized,
+{
+    fn bounding_box(&self) -> Rectangle {
+        Rectangle::new(Point::new(0, 0), Size::new(DISPLAY_WIDTH, DISPLAY_HEIGHT))
     }
 }
 
-impl<I2C> DrawTarget for Sh1107g<I2C> {
-    // DrawTarget define color dimension (monochro OLED = BinaryColor)
+// DrawTarget define color dimension (monochro OLED = BinaryColor)
+impl<'a, I2C, L> DrawTarget for Sh1107g<'a, I2C, L>
+where
+    L: Logger + ?Sized,
+{
     type Color = BinaryColor;
-    type Error = Infallible; // embedded-halのI2Cエラーをそのまま使う
-    
+    type Error = Infallible;
+
     /// ピクセルを描画する主要なメソッド
     fn draw_iter<PIXELS>(&mut self, pixels: PIXELS) -> Result<(), Self::Error>
     where
