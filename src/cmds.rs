86365f1ec8a2d4c4149bdc2cb8840ac00b8ff467
Placeless <thinkoverheat@gmail.com>
2025-08-07 12:37:44 +0900

    Move the interpreter from the debugger

--- src/cmds.rs
+++ src/cmds.rs
@@ -6,6 +6,9 @@
   個々のコマンドをsend_commandで送信するのがより確実。
 */
 
+#[cfg(feature = "debug_log")]
+use dvcdbg::logger::Logger;
+
 pub const DISPLAY_OFF: u8 = 0xAE;
 pub const DISPLAY_ON: u8 = 0xAF;
 pub const SET_MULTIPLEX_RATIO: u8 = 0xA8;
@@ -32,3 +35,33 @@ pub const SET_COM_PINS_CMD: u8 = 0xDA;
 pub const SET_COM_PINS_DATA: u8 = 0x12;
 pub const SET_ENTIRE_DISPLAY_ON_OFF_CMD: u8 = 0xA4;
 pub const SET_NORMAL_INVERSE_DISPLAY_CMD: u8 = 0xA6;
+
+#[cfg(feature = "debug_log")]
+/// デバッグ用の初期化コマンド列をログ出力する
+pub fn log_init_sequence<L: Logger>(logger: &mut L) {
+    log_cmd(logger, DISPLAY_OFF);
+    log_cmd(logger, SET_MULTIPLEX_RATIO);
+    log_cmd(logger, MULTIPLEX_RATIO_DATA);
+    log_cmd(logger, CHARGE_PUMP_ON_CMD);
+    log_cmd(logger, CHARGE_PUMP_ON_DATA);
+    log_cmd(logger, PAGE_ADDRESSING_CMD);
+    log_cmd(logger, SEGMENT_REMAP);
+    log_cmd(logger, COM_OUTPUT_SCAN_DIR);
+    log_cmd(logger, DISPLAY_START_LINE_CMD);
+    log_cmd(logger, DISPLAY_START_LINE_DATA);
+    log_cmd(logger, CONTRAST_CONTROL_CMD);
+    log_cmd(logger, CONTRAST_CONTROL_DATA);
+    log_cmd(logger, DISPLAY_OFFSET_CMD);
+    log_cmd(logger, DISPLAY_OFFSET_DATA);
+    log_cmd(logger, PRECHARGE_CMD);
+    log_cmd(logger, PRECHARGE_DATA);
+    log_cmd(logger, VCOM_DESELECT_CMD);
+    log_cmd(logger, VCOM_DESELECT_DATA);
+    log_cmd(logger, CLOCK_DIVIDE_CMD);
+    log_cmd(logger, CLOCK_DIVIDE_DATA);
+    log_cmd(logger, SET_COM_PINS_CMD);
+    log_cmd(logger, SET_COM_PINS_DATA);
+    log_cmd(logger, SET_ENTIRE_DISPLAY_ON_OFF_CMD);
+    log_cmd(logger, SET_NORMAL_INVERSE_DISPLAY_CMD);
+    log_cmd(logger, DISPLAY_ON);
+}
