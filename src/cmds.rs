a5fb69e0f98f224a4ef789c96c97ab5b95ddd77d
Placeless <thinkoverheat@gmail.com>
2025-08-25 10:09:35 +0900

    /

--- src/cmds.rs
+++ src/cmds.rs
@@ -17,3 +17,27 @@ pub fn set_start_line(line: u8) -> [u8; 2] {
 pub fn display_offset(offset: u8) -> [u8; 2] {
     [0xD3, offset]
 }
+
+pub const fn cmd(byte: u8) -> [u8; 1] {
+    [byte]
+}
+
+/*
+pub const SH1107G_INIT_CMDS: &[u8] = &[
+    0xAE, // Display OFF
+    0xDC, 0x00, // Display start line = 0
+    0x81, 0x2F, // Contrast
+    0x20,  0x02, // Memory addressing mode: page
+    0xA0, // Segment remap normal
+    0xC0, // Common output scan direction normal
+    0xA4, // Entire display ON from RAM
+    0xA6, // Normal display
+    0xA8, 0x7F, // Multiplex ratio 128
+    0xD3, 0x60, // Display offset
+    0xD5, 0x51, // Oscillator frequency
+    0xD9, 0x22, // Pre-charge period
+    0xDB, 0x35, // VCOM deselect level
+    0xAD, 0x8A, // DC-DC control
+    0xAF,       // Display ON
+];
+*/
